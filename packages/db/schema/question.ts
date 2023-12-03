d68ca04e1e04f484e780a307478c1c77a49170de
Rami Maalouf <rami.maalouf@ibm.com>
2023-12-03 09:14:51 -0500

    Schema has been fixed

--- packages/db/schema/question.ts
+++ packages/db/schema/question.ts
@@ -1,12 +1,13 @@
-import { sql } from "drizzle-orm";
-import { serial, int, text, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { relations, sql } from "drizzle-orm";
+import { int, serial, text, timestamp, varchar } from "drizzle-orm/mysql-core";
+
 import { mySqlTable } from "./_table";
-import { users, friends } from "./user";
+import { friends, users } from "./user";
 
 // Question
 export const questions = mySqlTable("question", {
   id: serial("id").primaryKey(),
-  createdByUserId: int("created_by_user_id").references(() => users.id),
+  createdByUserId: int("created_by_user_id").notNull(),
   text: text("text").notNull(),
   createdDatetime: timestamp("created_datetime")
     .default(sql`CURRENT_TIMESTAMP`)
@@ -16,18 +17,65 @@ export const questions = mySqlTable("question", {
 // Tag
 export const tags = mySqlTable("tag", {
   id: serial("id").primaryKey(),
-  createdByUserId: int("created_by_user_id").references(() => users.id),
+  createdByUserId: int("created_by_user_id").notNull(),
   tagName: varchar("email", { length: 30 }).notNull(),
 });
 
 // QuestionTag
 export const questionTags = mySqlTable("question_tag", {
-  questionId: int("question_id").references(() => questions.id),
-  tagId: int("tag_id").references(() => tags.id),
+  questionId: int("question_id"),
+  tagId: int("tag_id"),
 });
 
 // QuestionFriend
 export const questionFriends = mySqlTable("question_friend", {
-  questionId: int("question_id").references(() => questions.id),
-  friendId: int("friend_id").references(() => friends.id),
+  questionId: int("question_id"),
+  friendId: int("friend_id"),
 });
+export const questionsRelations = relations(questions, ({ one, many }) => ({
+  questionTags: many(questionTags),
+  questionFriends: many(questionFriends),
+  createdByUser: one(users, {
+    relationName: "Id of user who created question",
+    fields: [questions.createdByUserId],
+    references: [users.id],
+  }),
+}));
+
+export const tagsRelations = relations(tags, ({ one, many }) => ({
+  questionTags: many(questionTags),
+  createdByUser: one(users, {
+    relationName: "Id of user who created tag",
+    fields: [tags.createdByUserId],
+    references: [users.id],
+  }),
+}));
+
+export const questionTagsRelations = relations(questionTags, ({ one }) => ({
+  question: one(questions, {
+    relationName: "Id of question",
+    fields: [questionTags.questionId],
+    references: [questions.id],
+  }),
+  tag: one(tags, {
+    relationName: "Id of tag",
+    fields: [questionTags.tagId],
+    references: [tags.id],
+  }),
+}));
+
+export const questionFriendsRelations = relations(
+  questionFriends,
+  ({ one }) => ({
+    question: one(questions, {
+      relationName: "Id of question",
+      fields: [questionFriends.questionId],
+      references: [questions.id],
+    }),
+    friend: one(friends, {
+      relationName: "Id of friend",
+      fields: [questionFriends.friendId],
+      references: [friends.id],
+    }),
+  }),
+);
--- packages/db/schema/user.ts
+++ packages/db/schema/user.ts
@@ -1,7 +1,8 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { int, serial, text, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 import { mySqlTable } from "./_table";
+import { questionFriends, questions, tags } from "./question";
 
 // User
 export const users = mySqlTable("user", {
@@ -20,10 +21,35 @@ export const users = mySqlTable("user", {
 // Friend
 export const friends = mySqlTable("friend", {
   id: serial("id").primaryKey(),
-  createdByUserId: int("created_by_user_id").references(() => users.id),
-  friendUserId: int("friend_user_id").references(() => users.id),
+  createdByUserId: int("created_by_user_id").notNull(),
+  friendUserId: int("friend_user_id"),
   name: text("name").notNull(),
   createdDatetime: timestamp("created_datetime")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
 });
+
+// Relations
+export const usersRelations = relations(users, ({ many }) => ({
+  friends: many(friends, { relationName: "Id of user who created friend" }),
+  questions: many(questions, {
+    relationName: "Id of user who created question",
+  }),
+  tags: many(tags, { relationName: "Id of user who created tag" }),
+}));
+
+export const friendsRelations = relations(friends, ({ one, many }) => ({
+  user: one(users, {
+    relationName: "Id of user who created friend",
+    fields: [friends.createdByUserId],
+    references: [users.id],
+  }),
+  friend: one(users, {
+    relationName: "Id of friend user (optional)",
+    fields: [friends.friendUserId],
+    references: [users.id],
+  }),
+  questions: many(questionFriends, {
+    relationName: "Id of friend",
+  }),
+}));
