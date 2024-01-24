6e54d2017692735c95523fd4d510ac60f0d06bcf
Rami Maalouf <rami.maalouf@ibm.com>
2024-01-23 21:53:30 -0500

    added the number of questions a

--- packages/app/features/friends/FriendCard.tsx
+++ packages/app/features/friends/FriendCard.tsx
@@ -3,6 +3,7 @@ import { Link } from 'solito/link';
 import { type ComponentPropsWithoutRef, type FC } from 'react';
 import { XStack, Checkbox, YStack, Text } from '@acme/ui';
 import { formatDate, getQuestionsFromFriendId } from '../../lib/utils';
+import { MessageCircleQuestion } from '@tamagui/lucide-icons';
 
 interface Props extends ComponentPropsWithoutRef<"div"> {
   friend: RouterOutputs["friend"]["all"][number];
@@ -18,12 +19,18 @@ const Component: FC<Props> = (props) => {
 
   return (
     <Link href={`/friends/${friend.id.toString()}`}>
-      <XStack minHeight="$6" px="$4" ai="center" justifyContent="space-between">
-        <XStack gap={"$3"}>
-          <Text fontSize={16} fontWeight="bold">
+      <XStack px="$4" py="$4" ai="center" justifyContent="space-between">
+        <YStack gap={6}>
+          <Text fontSize={20} fontWeight="bold">
             {friend.name}
           </Text>
-        </XStack>
+          <XStack gap={6} ai="center">
+            <Text color="$secondaryColor">
+              {questionCount}
+            </Text>
+            <MessageCircleQuestion size={15} color="$secondaryColor" strokeWidth={2.5} />
+          </XStack>
+        </YStack>
         <YStack gap="$1.5">
           <Text
             // style={styles.subTitle}
