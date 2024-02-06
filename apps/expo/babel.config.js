a7b091257813b64c5ff423de201523e709db1bff
Rami Maalouf <rami.maalouf@ibm.com>
2024-02-05 23:31:16 -0500

    pull some changes from create-t3-turbo

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,6 +1,6 @@
 /** @type {import("@babel/core").ConfigFunction} */
 module.exports = function (api) {
-  api.cache.forever();
+  api.cache(true);
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
     plugins: [
@@ -17,19 +17,19 @@ module.exports = function (api) {
         },
       ],
       // if you want reanimated support
-      '@babel/plugin-transform-export-namespace-from',
+      // '@babel/plugin-transform-export-namespace-from',
       'react-native-reanimated/plugin',
       ...(process.env.EAS_BUILD_PLATFORM === 'android'
         ? []
         : [
-            [
-              '@tamagui/babel-plugin',
-              {
-                components: ['@acme/ui', 'tamagui'],
-                config: './tamagui.config.ts',
-              },
-            ],
-          ]),
+          [
+            '@tamagui/babel-plugin',
+            {
+              components: ['@acme/ui', 'tamagui'],
+              config: './tamagui.config.ts',
+            },
+          ],
+        ]),
       'transform-inline-environment-variables',
     ],
   };
