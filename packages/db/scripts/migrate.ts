8f0f4f778725ce130c72889b8c0c8095813fb8cd
Rami Maalouf <rami.maalouf@ibm.com>
2024-02-25 16:26:53 -0500

    move migrate file

--- packages/db/scripts/migrate.ts
+++ packages/db/scripts/migrate.ts
@@ -1,12 +1,13 @@
 import * as dotenv from 'dotenv';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 
-import { connection, db } from '.';
+import { createConnection } from '../utils';
 
 dotenv.config({
   path: '../../.env',
 });
+const { db } = createConnection();
 // This will run migrations on the database, skipping the ones already applied
-migrate(db, { migrationsFolder: './drizzle' });
+void migrate(db, { migrationsFolder: './drizzle' });
 // Don't forget to close the connection, otherwise the script will hang
 // await connection.end();
