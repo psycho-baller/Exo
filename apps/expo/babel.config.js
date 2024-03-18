c31bd343788eb42561e5f6f3ad29d4b93016fda0
Rami Maalouf <rami.maalouf@ibm.com>
2024-03-18 02:28:13 -0400

    now it works with android finally

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -4,9 +4,6 @@ module.exports = function (api) {
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
     plugins: [
-      // if you want reanimated support
-      // '@babel/plugin-transform-export-namespace-from',
-      'react-native-reanimated/plugin',
       ...(process.env.EAS_BUILD_PLATFORM === 'android'
         ? []
         : [
@@ -18,6 +15,9 @@ module.exports = function (api) {
             },
           ],
         ]),
+      // if you want reanimated support
+      // '@babel/plugin-transform-export-namespace-from',
+      // 'react-native-reanimated/plugin',
     ],
   };
 };
--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,19 +1,19 @@
 import '@expo/metro-runtime';
 
-// import { App } from 'expo-router/build/qualified-entry';
-// import { renderRootComponent } from 'expo-router/build/renderRootComponent';
+import { App } from 'expo-router/build/qualified-entry';
+import { renderRootComponent } from 'expo-router/build/renderRootComponent';
 
-// // This file should only import and register the root. No components or exports
-// // should be added here.
-// renderRootComponent(App);
+// This file should only import and register the root. No components or exports
+// should be added here.
+renderRootComponent(App);
 
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
+// import { registerRootComponent } from 'expo';
+// import { ExpoRoot } from 'expo-router';
 
-// Must be exported or Fast Refresh won't update the context
-export function App() {
-  const ctx = require.context('./src/app');
-  return <ExpoRoot context={ctx} />;
-}
+// // Must be exported or Fast Refresh won't update the context
+// export function App() {
+//   const ctx = require.context('./src/app');
+//   return <ExpoRoot context={ctx} />;
+// }
 
-registerRootComponent(App);
+// registerRootComponent(App);
