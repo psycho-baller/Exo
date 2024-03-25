74ef3342cf084059967e8bdc9b360dbd71892f75
Rami Maalouf <rami.maalouf@ibm.com>
2024-03-24 23:23:03 -0400

    added drizzle eslint that's provided by drizzle themselves

---
+++ tooling/eslint/drizzle.js
@@ -0,0 +1,17 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  root: true,
+  extends: [
+    "plugin:drizzle/all",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'drizzle',
+  ],
+  rules: {},
+};
+
+module.exports = config;
