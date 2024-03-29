fa47bc9d8a0924d0951180be509031eb42ecd73c
Rami Maalouf <rami.maalouf@ibm.com>
2024-03-29 00:29:17 -0400

    more cleanup

--- packages/db/scripts/drop_all_tables.sh
+++ packages/db/scripts/drop_all_tables.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-# Make sure to set the following environment variables and source them before running this script:
-# - DB_USER
-# - DB_NAME
-# - DB_HOST (if you're not using localhost)
-# - MYSQL_PWD (if you have a password set for the MySQL user)
-user=$DB_USER
-db=$DB_NAME
-host=$DB_HOST
+# ! This currently doesn't work with turso
+# Make sure to set the following environment variable and source it before running this script:
+# - DB_PATH: the path to your SQLite database file
+
+db=$DB_PATH
 
 echo "Dropping all tables from $db..."
 
 # Create a variable with the command to list all tables
-tables=$(mysql -u $user -h $host -Nse 'SHOW TABLES' $db)
+tables=$(sqlite3 $db ".tables")
 
 # Loop through the tables and drop each one
 for table in $tables; do
   echo "Dropping $table from $db..."
-  mysql -u $user -h $host -e "DROP TABLE \`$table\`" $db
+  sqlite3 $db "DROP TABLE $table"
 done
 
 echo "All tables dropped from $db."
--- packages/db/utils.ts
+++ packages/db/utils.ts
@@ -13,13 +13,6 @@ export function createConnection(): ConnectionResult {
   const client = createClient({
     url: 'http://127.0.0.1:8080',
   });
-  // export const connection = await mysql.createConnection({
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  //   multipleStatements: true,
-  // });
   const db = drizzle(client, { schema });
 
   return {
