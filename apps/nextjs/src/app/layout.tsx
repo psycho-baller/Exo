ed52e4eb3b872be55a3f4806104353302c6c3526
Rami Maalouf <rami.maalouf@ibm.com>
2024-04-11 01:28:33 -0400

    added styles coz why not

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { headers } from 'next/headers';
 
+import './globals.css';
+
 import { TamaguiProvider } from './TamaguiProvider';
 import { TRPCReactProvider } from './TRPCProviders';
 
