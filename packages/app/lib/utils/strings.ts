2bac29513181db5552cee0cdbb6d41c9827bc028
Rami Maalouf <rami.maalouf@ibm.com>
2024-04-19 10:38:01 -0400

    utils

---
+++ packages/app/lib/utils/strings.ts
@@ -0,0 +1,15 @@
+import { AppRouterKeys } from '@acme/api/src/root';
+
+export const getFullName = (firstName: string, lastName: string | null | undefined) => {
+  return lastName ? `${firstName} ${lastName}` : firstName;
+};
+
+type singularOptions = Exclude<AppRouterKeys, 'auth'>;
+export const getSingularFromPlural = (word: string): singularOptions => {
+  switch (word) {
+    case 'people':
+      return 'person';
+    default:
+      return word.replace(/s$/, '') as singularOptions;
+  }
+};
