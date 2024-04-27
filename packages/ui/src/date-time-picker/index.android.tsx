64f97c22b67363e35aa4a71837f5359fe3f1fde4
Rami Maalouf <rami.maalouf@ibm.com>
2024-04-26 23:55:14 -0400

    NEW: We now have an android specific date-time-picker

---
+++ packages/ui/src/date-time-picker/index.android.tsx
@@ -0,0 +1,44 @@
+import {DateTimePickerAndroid} from '@react-native-community/datetimepicker'
+import { Button, XStack, Text } from 'tamagui'
+import type { BaseProps, AndroidNativeProps } from '@react-native-community/datetimepicker'
+type Props = Omit<AndroidNativeProps, 'value' | 'onChange'> & {
+  value: Date | null
+  onChange: (date: Date) => Promise<void>
+}
+
+export const MyDateTimePicker = ({ value, onChange, ...props }: Props) => {
+
+  const showMode = (currentMode: 'date' | 'time') => {
+    DateTimePickerAndroid.open({
+      value: value ?? new Date(),
+      onChange: async (_, data) => {
+        if (data) {
+          await onChange(data)
+        }
+      },
+      mode: currentMode,
+      is24Hour: true,
+      ...props
+    });
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  return (
+    <XStack gap='$1.5' paddingVertical='$0'>
+      <Button fontWeight='100' size='$3' fontSize='$5' borderRadius='$lg' onPress={showDatepicker} >
+        {/* Apr 29, 2024 */}
+        {value?.toLocaleDateString()}
+      </Button>
+      <Button size='$3' fontSize='$5' borderRadius='$lg' onPress={showTimepicker}>
+        {value?.toLocaleTimeString().replace(/:\d+ /, ' ')}
+      </Button>
+    </XStack>
+  );
+};
--- packages/ui/src/date-time-picker/index.ios.tsx
+++ packages/ui/src/date-time-picker/index.ios.tsx
@@ -1,17 +1,18 @@
 import DateTimePicker from '@react-native-community/datetimepicker'
 import type { BaseProps } from '@react-native-community/datetimepicker'
-
+import { useState } from 'react'
+import { Platform } from 'react-native'
 type Props = Omit<BaseProps, 'value' | 'onChange'> & {
   value: Date | null
   onChange: (date: Date) => Promise<void>
 }
 
 export const MyDateTimePicker = ({ value, onChange, ...props }: Props) => {
+
   return (
     <DateTimePicker
       value={value ?? new Date()}
       mode='datetime'
-      // display='spinner'
       onChange={async (_, data) => {
         if (data) {
           await onChange(data)
