09f550b715957053d6073f4303f6e8010a34bbd6
Rami Maalouf <rami.maalouf@ibm.com>
2024-05-05 01:00:56 -0400

    using db and query as dep arrays because

--- packages/app/components/search/index.tsx
+++ packages/app/components/search/index.tsx
@@ -1,12 +1,32 @@
 import { useEffect, type FC } from 'react';
 import type { InputProps, ViewProps } from '@acme/ui';
 import { MyInput, UnstyledInput, View } from '@acme/ui';
-import { create, insert, insertMultiple, search } from '@orama/orama'
-import { useQuery } from '@tanstack/react-query';
+import { create, insertMultiple, search } from '@orama/orama'
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 interface Props extends Omit<InputProps, 'value'> {
   data: any[];
   schema: Record<string, unknown>;
   query: string;
+}
+
+
+
+const insertData = async (data: any, schema: Record<string, unknown>) => {
+  console.info("inserting data", data)
+  const db = await create({
+    schema: { ...schema } as const,
+  })
+  await insertMultiple(db, data)
+  return db;
+}
+const awaitSearch = async (db: any, query: string) => {
+  console.info("searching for", query)
+  const searchResult = await search(db, {
+    term: query,
+  })
+  // properties: ['question'],
+  console.log("searchResult.hits.document", searchResult.hits)
+  return searchResult.hits;
 }
 
 export const SearchInput: FC<Props> = ({
@@ -16,32 +36,24 @@ export const SearchInput: FC<Props> = ({
   query,
   ...rest
 }) => {
-
-  const insertData = async (data: any, schema: any) => {
-    const db = await create({
-      schema: { ...schema } as const,
-    })
-    await insertMultiple(db, data)
-    return db;
-  }
-  const awaitSearch = async (db: any, query: string) => {
-    const searchResult = await search(db, {
-      term: query,
-      // properties: ['question'],
-    })
-    console.log(searchResult.hits)
-    return searchResult.hits;
-  }
-
-  const { data: db } = useQuery({
+  const { data: db, error: DBError } = useQuery({
     queryKey: ['db', data, schema],
     queryFn: () => insertData(data, schema),
   })
-  useQuery({
-    queryKey: ['search', data, schema, query],
+
+  const { error } = useQuery({
+    queryKey: ['search', db, query],
     queryFn: () => awaitSearch(db, query),
-    enabled: !!query,
+    // enabled: !!query,
   })
+  if (DBError) {
+    console.error(DBError)
+    // return null
+  }
+  if (error) {
+    console.error(error)
+    // return null
+  }
 
   return (
     <UnstyledInput
