eeada105261bb40f32dbac92d02cffff8ff078f8
Rami Maalouf <rami.maalouf@ibm.com>
2024-05-18 13:26:15 -0400

    renamed to the appropriate naming

--- packages/app/components/Footer/SuperchargedInput.tsx
+++ packages/app/components/Footer/SuperchargedInput.tsx
@@ -1,119 +1,183 @@
-import { useState, useRef, useEffect } from 'react';
-import type { FC } from 'react';
-import { View, TextInput, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import type { NativeSyntheticEvent, TextInputKeyPressEventData } from 'react-native';
+import { UnstyledInput, Text, View, XStack } from '@acme/ui';
+import React, { useEffect, useState } from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+import type { NativeSyntheticEvent, TextInputKeyPressEventData, TextInputSelectionChangeEventData } from 'react-native';
 
-interface Props {
-  // tags: string[];
-  // setTags: (tags: string[]) => void;
-  [x: string]: any;
-}
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    height: 24,
+    position: 'relative',
+    alignSelf: 'center',
+  },
+  inputWrapper: {
+    position: 'absolute',
+    top: 0,
+    height: 24,
+    width: '100%',
+  },
+  input: {
+    height: 24,
+    width: '100%',
+  },
+  text: {
+    height: 24,
+    position: 'absolute',
+    top: 0,
+    color: 'transparent',
+  },
+  mention: {
+    backgroundColor: 'rgba(0, 150, 255, .5)',
+  }
+});
+// import chrono from 'chrono-node';
+type ReferenceType = 'person' | 'group' | 'topic' | 'date' | null;
+type SuperchargedWord = {
+  word: string;
+  enabled?: boolean;
+  reference: ReferenceType;
+  // active: boolean;
+};
 
-export const SuperchargedInput: FC<Props> = (props) => {
-  const { ...rest } = props;
+export const SuperchargedInput = () => {
+  const [inputWords, setInputWords] = useState<SuperchargedWord[]>([]);
+  const [selection, setSelection] = useState<{ start: number, end: number }>({ start: 0, end: 0 });
+  const [justDisabledWord, setJustDisabledWord] = useState(false);
 
-  const [tags, setTags] = useState<string[]>([]);
-  const [width, setWidth] = useState<number>(1);
-  const [focused, setFocused] = useState<boolean>(false);
-  const [inputVal, setInputVal] = useState<string>("");
-  const input = useRef<TextInput>(null);
+  const handleSelectionChange = (e: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
+    const newSelection = e.nativeEvent.selection;
+    setSelection(newSelection);
 
-  const removeTag = (i: number) => {
-    setTags([...tags.slice(0, i), ...tags.slice(i + 1)]);
+    // const inputText = inputWords.map(({ word }) => word).join('');
+    // const newInputWords = inputWords.map((word, index) => ({
+    //   ...word,
+    //   // active: index === activeWordIndex,
+    // }));
+    // setInputWords(newInputWords);
   };
 
+  const formatText = (inputWords: SuperchargedWord[]) => {
+    return inputWords.map(({ word, reference, enabled }, index) => {
+      if (reference === 'person' || reference === 'group') {
+        return (
+          <Text unstyled
+            key={index.toString() + word} style={enabled ? styles.mention : undefined}>
+            {word}
+          </Text>
+        );
+      }
+      if (reference === 'topic') {
+        return (
+          <Text unstyled
+            key={index.toString() + word} style={enabled ? styles.mention : undefined}>
+            {word}
+          </Text>
+        );
+      }
+      return word;
+    });
+  }
 
-  const inputKeyDown = (e: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
-    e.stopPropagation();
-    if ((e.nativeEvent.key === "Tab" || e.nativeEvent.key === "," || e.nativeEvent.key === "Enter") && inputVal) {
-      e.preventDefault();
-      if (tags.find((tag) => tag.toLowerCase() === inputVal.toLowerCase())) {
-        if (input.current) {
-          input.current.focus();
-        }
-        return;
-      }
-      setTags([...tags, inputVal.trim()]);
-      // console.log(input.current);
-      if (input.current) {
-        console.log(inputVal)
-        // input.current.clear();
-        setInputVal(prev => "");
-      }
-    } else if (e.nativeEvent.key === "Backspace" && !inputVal && tags.length) {
-      removeTag(tags.length - 1);
-      setInputVal(prev => prev + tags.slice(-1)[0]);
-    }
+  const handleChangeText = (newInput: string) => {
+    // Insert new input at current selection position
+    const before = inputWords.map(({ word }) => word).join('');
+    const after = inputWords.slice(selection.start).map(({ word }) => word).join('');
+    const updatedText = before + newInput + after;
+
+    setInputWords(addTextProperties(updatedText));
   };
 
-  useEffect(() => {
-    setWidth(Dimensions.get('window').width);
-  }, [inputVal]);
+  const addTextProperties: (text: string) => SuperchargedWord[] = (text) => {
+    const words = text.split(/(\s+)/)
+    return words.map((word, index) => {
+      let reference: ReferenceType = null;
+      if (word.startsWith('@')) {
+        reference = word.startsWith('@@') ? 'group' : 'person';
+      } else if (word.startsWith('#')) {
+        reference = 'topic';
+      }
+
+      return {
+        word,
+        enabled: reference !== null,
+        reference,
+        // active: false,
+      };
+    });
+  }
+
+
+  const handleBackspace = () => {
+    if (selection.start <= 0) return; // No action if cursor is at the start
+    // const activeWordIndex = inputWords.findIndex(({ active }) => active);
+    setInputWords((prevInputWords) => {
+      const inputText = prevInputWords.map(({ word }) => word).join('');
+      const activeWordIndex = inputText.slice(0, selection.start).split(/(\s+)/).length - 1;
+      const word = prevInputWords[activeWordIndex];
+      console.log('prevInputWords', prevInputWords);
+
+      if (word?.reference && word?.enabled) {
+        console.log('deleting reference', selection.start, activeWordIndex);
+        setJustDisabledWord(true);
+        return prevInputWords.map((w, index) =>
+          index === activeWordIndex ? { ...w, enabled: false } : w
+        );
+      }
+      console.log('deleting word', selection.start, activeWordIndex);
+      const indexToSlice = justDisabledWord ? selection.start + 1 : selection.start;
+
+      const newInputText = inputText.slice(0, indexToSlice - 1) + inputText.slice(indexToSlice);
+      justDisabledWord && setJustDisabledWord(false)
+      const newInputWords = addTextProperties(newInputText);
+      return newInputWords.map((newWord, index) => {
+        const oldWord = prevInputWords[index];
+        return oldWord && index === activeWordIndex ? { ...oldWord, word: newWord.word } : newWord;
+      });
+      // if (newInputWords[wordIndex]) {
+      //   newInputWords[wordIndex] = prevInputWords[wordIndex];
+      // }
+      // return newInputWords.map((word, index) => {
+      //   const oldWord = prevInputWords[index];
+      //   const isTheModifiedWord = index === activeWordIndex;
+      //   const updatedWord = newInputText.split(/(\s+)/)[activeWordIndex];
+      //   const wordExists = oldWord && updatedWord;
+      //   if (isTheModifiedWord && wordExists) {
+      //     return {
+      //       ...oldWord,
+      //       // active: true,
+      //       // enabled: false,
+      //       word: updatedWord,
+      //     };
+      //   }
+      //   return word;
+      // });
+    });
+  };
 
   return (
-    <View
-      style={styles.container}
-      onStartShouldSetResponder={() => {
-        if (input.current) {
-          input.current.focus();
-        }
-        return true;
-      }}
-    >
-      <View style={styles.tagContainer}>
-        {tags.map((tag, i) => (
-          <View key={tag} style={styles.tag}>
-            <Text>{tag}</Text>
-            <TouchableOpacity onPress={() => removeTag(i)}>
-              <Text>X</Text>
-            </TouchableOpacity>
-          </View>
-        ))}
-        <View style={styles.inputContainer}>
-          <TextInput
-            value={inputVal}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            onChangeText={(text) => {
-              setInputVal(text.replace(/,/g, ""));
-            }}
-            style={[styles.input]}
-            onKeyPress={inputKeyDown}
-            ref={input}
-            {...rest}
-          />
-        </View>
+    <View style={styles.wrapper}>
+      <Text unstyled
+        fontSize={28}
+
+        style={styles.text}>
+        {formatText(inputWords)}
+      </Text>
+      <View style={styles.inputWrapper}>
+        <UnstyledInput
+          fontSize={28}
+          onSelectionChange={handleSelectionChange}
+          style={styles.input}
+          value={inputWords.map(({ word }) => word).join('')}
+          onKeyPress={(e: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
+            console.log('key', e.nativeEvent.key);
+            if (e.nativeEvent.key === 'Backspace') {
+              handleBackspace();
+            } else {
+              handleChangeText(e.nativeEvent.key);
+            }
+          }}
+        />
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 10,
-  },
-  tagContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  tag: {
-    flexDirection: 'row',
-    padding: 5,
-    marginRight: 5,
-    marginBottom: 5,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 20,
-  },
-  inputContainer: {
-    flex: 1,
-  },
-  input: {
-    fontSize: 16,
-    backgroundColor: 'green',
-  },
-});
