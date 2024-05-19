c82f0b41a0a0a2d10c2d5665bc95b987c9bfcb51
Rami Maalouf <rami.maalouf@ibm.com>
2024-05-19 16:57:30 -0400

    CLEAN UP + enter key now submits the add Q bottom sheet

--- packages/app/components/Footer/AddQuestion.tsx
+++ packages/app/components/Footer/AddQuestion.tsx
@@ -7,7 +7,6 @@ import { Button, ErrorText, Label, BottomSheetInput, XStack, YStack, Text } from
 import { BottomSheet } from '../BottomSheet'
 
 import type { Topic } from '../../../db/schema/types'
-import { useAddPersonStore } from '../../stores/addQuestion'
 import { AddPerson } from './AddPerson'
 import { sheetRefAtom } from '../../atoms/addQuestion'
 import { useAtom } from 'jotai'
@@ -17,7 +16,6 @@ import { getFullName } from '../../utils/strings'
 
 export const AddQuestion: FC = () => {
   const utils = api.useUtils()
-  const [sheetRef] = useAtom(sheetRefAtom)
   const { data: allTopics } = api.topic.all.useQuery()
   const createTopicMutation = api.topic.create.useMutation({
     async onSuccess(data) {
@@ -29,73 +27,13 @@ export const AddQuestion: FC = () => {
   const { data: people } = api.person.all.useQuery();
   const { data: groups } = api.group.all.useQuery();
 
-
-  const [selectedPerson, setPersonSearch,
-    dropdownOpen,
-    setDropdownOpen
-  ] = useAddPersonStore(
-    (state) => [
-      state.selectedPerson,
-      state.setPersonSearch,
-      state.dropdownOpen,
-      state.setDropdownOpen,
-    ],
-  )
-
-  // const [question, setQuestion] = useState('')
-  const [question, setQuestion] = useAtom(superchargedInputWordsAtom);
-  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null)
-  const [showTopicSuggestions, setShowTopicSuggestions] = useState(false)
-  const [searchTerm, setSearchTerm] = useState('')
-  const [filteredTopics, setFilteredTopics] = useState<Topic[]>(allTopics ?? [])
-
-  // const searchTopics = api.topic.search.useQuery
-  // const { data: topics, isLoading } = searchTopics(
-  //   { query: searchTerm },
-  //   {
-  //     enabled: !!searchTerm,
-  //   },
-  // )
-
-  // useEffect(() => {
-  //   if (topics && !isLoading) {
-  //     setFilteredTopics(topics)
-  //   }
-  // }, [topics, isLoading])
-
-  // useEffect(() => {
-  //   const words = question.includes(' ') ? question.split(' ') : [question]
-  //   // if any of the words starts with a hashtag, show topic suggestions
-  //   // for (const word of words) {
-  //   //   if (word.startsWith('#')) {
-  //   //     setShowTopicSuggestions(true);
-  //   //     setSearchTerm(word.slice(1));
-  //   //     return;
-  //   //   }
-  //   // }
-  //   const lastWordWithKey = words.slice(-1)[0]
-  //   if (lastWordWithKey?.startsWith('#')) {
-  //     const lastWord = lastWordWithKey.slice(1)
-  //     setSearchTerm(lastWord)
-  //     // check if the last word is a topic
-  //     const topic = allTopics?.find((topic) => topic.name === lastWord)
-  //     // if exists, select it
-  //     if (topic) {
-  //       setSelectedTopic(topic)
-  //     } else {
-  //       setFilteredTopics(allTopics ? allTopics.filter((topic) => topic.name.includes(searchTerm)) : [])
-  //       setShowTopicSuggestions(true)
-  //     }
-  //   } else {
-  //     setShowTopicSuggestions(false)
-  //     setSearchTerm('')
-  //   }
-  // }, [question])
+  const [inputWords, setInputWords] = useAtom(superchargedInputWordsAtom);
+  const [sheetRef] = useAtom(sheetRefAtom)
 
   const { mutateAsync: mutateQuestion, error } = api.question.create.useMutation({
     async onSuccess(data) {
       sheetRef?.current?.close()
-      const topicWord = question.find((word) => word.reference === 'topic')?.word.slice(1).toLowerCase();
+      const topicWord = inputWords.find((word) => word.reference === 'topic')?.word.slice(1).toLowerCase();
       const selectedTopic = allTopics?.find((topic) => topic.name.toLowerCase() === topicWord);
       const createdQuestion = data[0]
       if (createdQuestion && selectedTopic) {
@@ -105,23 +43,20 @@ export const AddQuestion: FC = () => {
         })
       }
       // reset form
-      setQuestion([])
-      setSelectedTopic(null)
-      setDropdownOpen(false)
-      setPersonSearch('')
+      setInputWords([])
       await utils.question.all.invalidate()
     },
   })
 
   function addQuestion() {
     // find the person id from the selected person
-    const personWord = question.find((word) => word.reference === 'person')?.word.slice(1).toLowerCase();
+    const personWord = inputWords.find((word) => word.reference === 'person')?.word.slice(1).toLowerCase();
     const person = people?.find((person) => person.firstName.toLowerCase() === personWord);
 
-    const groupWord = question.find((word) => word.reference === 'group')?.word.slice(2).toLowerCase();
+    const groupWord = inputWords.find((word) => word.reference === 'group')?.word.slice(2).toLowerCase();
     const group = groups?.find((group) => group.name.toLowerCase() === groupWord);
 
-    const questionText = question
+    const questionText = inputWords
       .filter((word) => !word.reference)
       .map((word) => word.word)
       .join('')
@@ -134,44 +69,21 @@ export const AddQuestion: FC = () => {
     });
   }
 
-  const selectTopic = (topic: Topic) => {
-    // autocomplete the question with the selected topic
-    // setQuestion(question.replace(/#[\w]*$/, `#${topic.name}`))
-    setSelectedTopic(topic)
-    console.log('selectedTopic', topic)
-    setShowTopicSuggestions(false)
-  }
-
-  const createAndSelectTopic = (topicName: string) => {
-    createTopicMutation.mutate(
-      { name: topicName },
-      {
-        onSuccess: (topic) => {
-          selectTopic(topic[0] ?? { id: 0, name: '', createdByUserId: '' })
-        },
-      },
-    )
-  }
-
   return (
-    <BottomSheet sheetRefAtom={sheetRefAtom} snapPoints={['30%']}>
+    <BottomSheet sheetRefAtom={sheetRefAtom} snapPoints={['25%']}>
       <XStack justifyContent='space-between'>
         <Label fontSize={'$1'} unstyled color='$secondaryColor' htmlFor='question'>
           QUESTION
         </Label>
       </XStack>
       <XStack alignItems='center'>
-        {/* <BottomSheetInput
-          width={800}
-          fontSize={'$8'}
-          paddingVertical={'$2'}
-          marginBottom={'$4'}
-          autoFocus={dropdownOpen}
+        <SuperchargedInput
+          // paddingVertical={'$2'}
+          // marginBottom={'$4'}
           placeholder='Add Question'
-          value={question}
-          onChangeText={setQuestion}
-        /> */}
-        <SuperchargedInput placeholder='Add Person' />
+          onSubmitEditing={addQuestion}
+          autoFocus
+        />
         <Button justifyContent='flex-end' unstyled onPress={addQuestion}>
           <CheckCircle2 />
         </Button>
