a8cc912df89a43db2961bc6d2537af02e6e34b5e
Rami Maalouf <rami.maalouf@ibm.com>
2024-05-27 02:05:58 -0400

    chore: Remove unused import and export in db/index.ts

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -5,5 +5,3 @@ export const { db } = createConnection()
 export { sqliteTable as tableCreator } from './schema/_table'
 
 export * from 'drizzle-orm'
-
-export { useMigrations } from 'drizzle-orm/expo-sqlite/migrator'
