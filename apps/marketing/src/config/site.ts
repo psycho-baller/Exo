2d69309fdbed5d7e5490c5e194b74c1639610c04
Rami Maalouf <rami.maalouf@ibm.com>
2024-07-23 21:00:25 +0200

    Feat: There is a marketing page where users can join my discord server

---
+++ apps/marketing/src/config/site.ts
@@ -0,0 +1,23 @@
+import type { SiteConfig } from '~types/config'
+
+export const siteConfig: SiteConfig = {
+  name: 'SnipTube',
+  slogan: '',
+  hero: 'Spark meaningful conversations & enrich your relationships',
+  description: 'A perosnal ',
+  url: 'https://sniptube.tech',
+  ogImage: 'https://sniptube.tech/og.jpg',
+  links: {
+    // twitter: "https://twitter.com/",
+    github: 'https://github.com/psycho-baller/sniptube',
+    chrome: '/chrome',
+    firefox: '/firefox',
+    edge: '/edge',
+    safari: '/safari',
+    kofi: 'https://ko-fi.com/ramimaalouf',
+    linkedin: 'https://www.linkedin.com/in/rami--maalouf/',
+    youtube: 'https://youtube.com/@ramimaalouf',
+    instagram: 'https://www.instagram.com/psycho.baller/',
+    portfolio: 'https://rami-maalouf.tech',
+  },
+}
