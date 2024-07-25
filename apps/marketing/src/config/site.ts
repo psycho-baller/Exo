262033e2a768178fe5a219610c9b927db45a5635
Rami Maalouf <rami.maalouf@ibm.com>
2024-07-25 08:19:37 +0200

    typo

--- apps/marketing/src/config/site.ts
+++ apps/marketing/src/config/site.ts
@@ -1,26 +1,22 @@
 import type { SiteConfig } from '~types/config'
 
-export const siteConfig: SiteConfig = {
+export const siteConfig = {
   name: 'Roots',
   slogan: 'Build Lifelong Friendships Through Meaningful Conversations',
   hero: 'Build Lifelong Friendships Through Meaningful Conversations',
   description:
-    'Overcome social barriers and connect deeply with peers through our community and app', //Join our discord server and start making friends today!
+    'Overcome social barriers and connect deeply with peers through our community-based application', //Join our discord server and start making friends today!
   url: '',
   ogImage: '',
   links: {
     // twitter: "https://twitter.com/",
-    github: 'https://github.com/psycho-baller/',
+    github: 'https://github.com/psycho-baller/convo-topics',
     waitlist: 'https://survey.typeform.com/to/erptGdCr',
     discord: 'https://discord.gg/m8GM7bHp',
-    chrome: '/chrome',
-    firefox: '/firefox',
-    edge: '/edge',
-    safari: '/safari',
     kofi: 'https://ko-fi.com/ramimaalouf',
     linkedin: 'https://www.linkedin.com/in/rami--maalouf/',
     youtube: 'https://youtube.com/@ramimaalouf',
     instagram: 'https://www.instagram.com/psycho.baller/',
     portfolio: 'https://rami-maalouf.tech',
   },
-}
+} as const
