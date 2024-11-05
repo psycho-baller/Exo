2c6efdfd9226f5f68689923014e9a9f73455371a
Rami Maalouf <rami.rami@ucalgary.ca>
2024-11-05 08:00:53 -0700

    Added bt advertising for android

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -21,6 +21,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     bundleIdentifier: 'com.convo-topics',
     supportsTablet: true,
+    // needs membership to Apple Developer Program to use Push Notifications
+    infoPlist: {
+      NSBluetoothAlwaysUsageDescription: 'This app uses Bluetooth to scan for nearby devices.',
+      UIBackgroundModes: ['bluetooth-central', 'location', 'fetch', 'remote-notification'],
+    },
   },
   android: {
     package: 'com.convo_topics',
@@ -29,6 +34,16 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       foregroundImage: './assets/icon.png',
       backgroundColor: '#0D23FF',
     },
+    permissions: [
+      'android.permission.BLUETOOTH',
+      'android.permission.BLUETOOTH_ADMIN',
+      'android.permission.BLUETOOTH_SCAN',
+      'android.permission.BLUETOOTH_CONNECT',
+      'android.permission.ACCESS_COARSE_LOCATION',
+      'android.permission.ACCESS_FINE_LOCATION',
+      'android.permission.ACCESS_BACKGROUND_LOCATION',
+      'android.permission.BLUETOOTH_ADVERTISE',
+    ],
   },
   web: {
     bundler: 'metro',
@@ -42,5 +57,15 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     tsconfigPaths: true,
     typedRoutes: true,
   },
-  plugins: ['expo-router'],
+  plugins: [
+    'expo-router',
+    [
+      '@config-plugins/react-native-ble-plx',
+      {
+        isBackgroundEnabled: true,
+        modes: ['peripheral', 'central'],
+        bluetoothAlwaysPermission: 'Allow $(PRODUCT_NAME) to connect to bluetooth devices',
+      },
+    ],
+  ],
 })
