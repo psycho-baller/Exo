a35ead6909688e3152aebe2f465a8a597439e492
Rami Maalouf <rami.rami@ucalgary.ca>
2024-11-06 10:19:46 -0700

    not even using that shit

--- packages/app/provider/trpc/index.tsx
+++
@@ -1,5 +0,0 @@
-import { TRPCProvider as TRPCProviderOG } from '../../../server-api/utils/provider'
-
-export const TRPCProvider = ({ children }: { children: React.ReactNode }): React.ReactNode => {
-  return <>{children}</>
-}
