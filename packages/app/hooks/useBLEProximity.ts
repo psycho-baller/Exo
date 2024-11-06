8fe64afbfd4d5551dae04be2e5285c88168fd673
Rami Maalouf <rami.rami@ucalgary.ca>
2024-11-06 10:22:48 -0700

    NEW: APP NOW ADVRTISES ITSELF TO OTHER BLE LISTENERS

--- packages/app/hooks/useBLEProximity.ts
+++ packages/app/hooks/useBLEProximity.ts
@@ -1,116 +1,176 @@
-import { useEffect, useMemo, useState } from 'react'
-import { BleManager, type Device, State } from 'react-native-ble-plx'
-import { PermissionsAndroid, Platform } from 'react-native'
+import { useEffect, useMemo, useState, useCallback } from 'react'
+import { BleManager, type Device, State, type BleError, DeviceId } from 'react-native-ble-plx'
+import * as ExpoDevice from 'expo-device'
 import { requestPermissions } from '../utils/permissions'
 import { sendLocalNotification } from './usePushNotifications'
-// import { useProximityDetectionEnabled } from '../atoms/ble'
-import { registerBackgroundFetch, unregisterBackgroundFetch } from '../utils/backgroundBle'
+import { getDeviceId, startAdvertisingHelper, unregisterBackgroundBleTask } from '../utils/backgroundBle'
+
+const SCAN_TIMEOUT = 10000 // 10 seconds
+const RETRY_INTERVAL = 60000 // 1 minute
+const COMPANY_ID = 0x12D
+
+const MAJOR = 1234;
+const MINOR = 4321;
+const SHOULD_ADVERTISE = true;
+
 
 const useBLEProximity = () => {
   const [scannedDevices, setScannedDevices] = useState<Device[]>([])
-  const [followedIDs, setFollowedIDs] = useState<string[]>([])
   const bleManager = useMemo(() => new BleManager(), [])
-  // const [isEnabled, setEnabled] = useProximityDetectionEnabled()
   const isEnabled = true
-  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
-    devices.findIndex((device) => nextDevice.id === device.id) > -1
 
-  const scanForPeripherals = async () => {
+  const isDuplicateDevice = useCallback(
+    (devices: Device[], nextDevice: Device) =>
+      devices.findIndex((device) => nextDevice.id === device.id) > -1,
+    [],
+  )
+  const alreadyFound = useCallback(
+    (arr: Device[], device: Device) => arr.find((d) => d.id === device.id),
+    [],
+  )
+  const scanForPeripherals = useCallback(async () => {
+    const devices: Device[] = []
+    let isScanning = false
     try {
-      // await bleManager.enable() // This will prompt the user to enable Bluetooth if it's not already on
-      // Check if Bluetooth is powered on
       const state = await bleManager.state()
       if (state !== State.PoweredOn) {
         sendLocalNotification('BLE Error', `Bluetooth is not powered on. Current state: ${state}`)
-        return
+        return devices
       }
+      isScanning = true
 
-      const devices: Device[] = []
-      bleManager.startDeviceScan(null, null, (error, device) => {
-        if (error) {
-          console.error('Error Code:', error.errorCode)
-          console.error('Error Message:', error.message)
-          console.error('Error Reason:', error.reason)
-          sendLocalNotification('Background BLE scan', `Error scanning for devices: ${error}`)
-          console.log(error)
-        }
-        if (device) {
-          devices.push(device)
-          // if (followedIDs.includes(device.id)) {
-          setScannedDevices((prevState: Device[]) => {
-            if (!isDuplicteDevice(prevState, device)) {
-              return [...prevState, device]
+      return new Promise<Device[]>((resolve) => {
+        bleManager.startDeviceScan(
+          null,
+          {
+            // legacyScan: false,
+            // scanMode: ScanMode.LowLatency,
+          },
+          (error, device) => {
+            if (error) {
+              handleScanError(error)
+              isScanning = false
+              resolve(devices)
+              return
             }
-            return prevState
-          })
-          // alert(`You're near ${device.name || 'a followed user'}!`)
-          // }
-        }
+            if (device && !isDuplicateDevice(devices, device) && !alreadyFound(devices, device)) {
+              devices.push(device)
+              // setScannedDevices((prevDevices) => [...prevDevices, device])
+            }
+            // if (device) {
+            //   setScannedDevices((prevState: Device[]) => {
+            //     if (!isDuplicateDevice(prevState, device) && !alreadyFound) {
+            //       return [...prevState, device]
+            //     }
+            //     return prevState
+            //   })
+            // }
+          },
+        )
+
+        // Stop scan after SCAN_TIMEOUT
+        setTimeout(() => {
+          if (isScanning) {
+            bleManager.stopDeviceScan()
+            isScanning = false
+            console.log('Scan completed. Found devices:', devices.length)
+            resolve(devices)
+          }
+        }, SCAN_TIMEOUT)
       })
-      sendLocalNotification(
-        'Background BLE scan',
-        `Found ${devices.length} devices, ${devices.map((d) => d.name).join(', ')}, ${devices.map(
-          (d) => d.id,
-        )}`,
-      )
     } catch (error) {
       console.error('Error starting BLE scan:', error)
-      sendLocalNotification('BLE Error', `Failed to start BLE scan: ${error.message}`)
+      sendLocalNotification('BLE Error', `Failed to start BLE scan: ${error}`)
+      return devices
     }
-  }
+  }, [bleManager, isDuplicateDevice, alreadyFound])
 
-  // const connectToDevice = async (device: Device) => {
-  //   try {
-  //     const deviceConnection = await bleManager.connectToDevice(device.id)
-  //     setConnectedDevice(deviceConnection)
-  //     await deviceConnection.discoverAllServicesAndCharacteristics()
-  //     bleManager.stopDeviceScan()
-  //     startStreamingData(deviceConnection)
-  //   } catch (e) {
-  //     console.log('FAILED TO CONNECT', e)
-  //   }
-  // }
+  const handleScanError = useCallback(
+    (error: BleError) => {
+      console.error('BLE Scan Error:', error)
+      console.error('Error Code:', error.errorCode)
+      console.error('Error Message:', error.message)
+      console.error('Error Reason:', error.reason)
+      sendLocalNotification('BLE Scan Error', `${error.message}. Retrying in 1 minute.`)
 
-  // const disconnectFromDevice = () => {
-  //   if (connectedDevice) {
-  //     bleManager.cancelDeviceConnection(connectedDevice.id)
-  //     setConnectedDevice(null)
-  //     setHeartRate(0)
-  //   }
-  // }
+      if (error.errorCode === 600) {
+        // Schedule a retry after RETRY_INTERVAL
+        setTimeout(() => {
+          scanForPeripherals()
+        }, RETRY_INTERVAL)
+      }
+    },
+    [scanForPeripherals],
+  )
 
-  const requestBLEPermissions = async () => {
+  const requestBLEPermissions = useCallback(async () => {
     const permissionsGranted = await requestPermissions()
     if (!permissionsGranted) {
       alert('Bluetooth permissions are required to use this feature')
       return false
     }
     return true
-  }
+  }, [])
+
+  const startAdvertising = useCallback(async () => {
+    if (SHOULD_ADVERTISE) {
+      const UUID = "44C13E43-097A-9C9F-537F-5666A6840C08"
+      try {
+        await startAdvertisingHelper(UUID, MAJOR, MINOR, COMPANY_ID)
+        //   // Optional: Add additional advertising options here
+        //   localName: 'MyBLEDevice', // Optional: Set a local name for the advertisement
+        // })
+        console.log('Started advertising with UUID:', UUID)
+      } catch (error) {
+        console.error('Error starting advertising:', error)
+      }
+    }
+  }, [])
+
   useEffect(() => {
     const initializeBLE = async () => {
       const granted = await requestBLEPermissions()
-      console.log('granted', granted)
+      console.log('Permissions granted:', granted)
       if (!granted) return
 
-      console.log('registering background fetch: ', isEnabled)
+      console.info('Initializing BLE scan:', isEnabled)
       if (isEnabled) {
-        // await registerBackgroundFetch()
-        await scanForPeripherals()
+        await startAdvertising() // Start advertising if enabled
+        const deviceScanResults = await scanForPeripherals()
+        const devices = deviceScanResults.filter((d) => d.id)
+        // const devicesWithNames = devices.filter((d) => d.name || d.localName)
+
+        // for (const d of devices) {
+        //   console.error(
+        //     'Device:',
+        //     `Found device: ${d.id}
+        //   name: ${d.name}
+        //   localName: ${d.localName}
+        //   rssi: ${d.rssi}
+        //   serviceUUIDs: ${d.serviceUUIDs}
+        //   manufacturerData: ${d.manufacturerData}
+        //   serviceData: ${d.serviceData}
+        //   mtu: ${d.mtu}
+        //   txPowerLevel: ${d.txPowerLevel}
+        //   isConnectable: ${d.isConnectable}
+        //   solicitedServiceUUIDs: ${d.solicitedServiceUUIDs}
+        //   overflowServiceUUIDs: ${d.overflowServiceUUIDs}`,
+        //   )
+        // }
+        sendLocalNotification('BLE Scan result', `Found ${deviceScanResults.length} devices`)
       } else {
-        await unregisterBackgroundFetch()
+        await unregisterBackgroundBleTask()
         bleManager.stopDeviceScan()
       }
     }
 
-    initializeBLE()
-    // Clean up the BLE scan on component unmount
+    void initializeBLE()
+
     return () => {
       bleManager.stopDeviceScan()
     }
-  }, [bleManager, requestBLEPermissions, scanForPeripherals, isEnabled])
+  }, [bleManager, requestBLEPermissions, scanForPeripherals, startAdvertising])
 
-  // Return the list of scanned devices and any additional data you want to expose
   return { scannedDevices }
 }
 
