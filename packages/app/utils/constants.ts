a3eaf23670a6a74752ea057ec3f3f49ca1d923f8
Rami Maalouf <rami.rami@ucalgary.ca>
2024-11-06 23:55:25 -0700

    failed attempt to decode but also using constants

--- packages/app/utils/constants.ts
+++ packages/app/utils/constants.ts
@@ -1,1 +1,11 @@
 export const CARD_HEIGHT = 70
+
+// BLE constants
+export const SCAN_TIMEOUT = 10000 // 10 seconds
+export const RETRY_INTERVAL = 60000 // 1 minute
+export const COMPANY_ID = 0x0E0
+export const MAJOR = 1234;
+export const MINOR = 4321;
+export const SHOULD_ADVERTISE = true;
+
+export const BACKGROUND_BLE_TASK = 'background-ble-scan'
