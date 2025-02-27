c4563cf1dbdb8c9fe903ca34f2cd03e867c4292d
Rami Maalouf <rami.rami@ucalgary.ca>
2025-02-26 17:47:31 -0700

    added waitlist link

--- apps/marketing/src/components/Hero/index.tsx
+++ apps/marketing/src/components/Hero/index.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
         <div className="space-x-4">
           <SecondaryButtonLink href={siteConfig.links.discord} className={styles.cta}>Join Discord Server</SecondaryButtonLink>
           <Link
-            href="/#waitlist"
+            href="https://app.formbricks.com/s/l192p9vlmbuj2kjo673vl38s"
             className={cn(buttonVariants({ size: "lg" }))}
           >
             Join Waitlist
