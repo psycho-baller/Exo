0086c0bec2e2570ccb1834f1dc590fbaa62b7c4b
Rami Maalouf <rami.rami@ucalgary.ca>
2025-03-13 09:57:52 -0600

    update wording

--- apps/marketing/src/components/Hero/index.tsx
+++ apps/marketing/src/components/Hero/index.tsx
@@ -25,12 +25,12 @@ const Hero = () => {
         <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-7xl">{siteConfig.hero}</h1>
         <p className="max-w-[42rem] !text-manatee leading-normal sm:text-xl sm:leading-8">{siteConfig.description}</p>
         <div className="space-x-4">
-          <SecondaryButtonLink href={siteConfig.links.discord} className={styles.cta}>Join Discord Server</SecondaryButtonLink>
+          <SecondaryButtonLink href={siteConfig.links.discord} className={styles.cta}>Join Discord Community</SecondaryButtonLink>
           <Link
             href={siteConfig.links.waitlist}
             className={cn(buttonVariants({ size: "lg" }))}
           >
-            Join Waitlist
+            Join App Waitlist
           </Link>
         </div>
       </div>
