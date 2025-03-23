03416809029c51ab0de924673fd898c5d5bde59f
Rami Maalouf <rami.rami@ucalgary.ca>
2025-03-23 16:55:28 -0600

    Updated Discord link

--- apps/marketing/src/config/site.ts
+++ apps/marketing/src/config/site.ts
@@ -12,7 +12,7 @@ export const siteConfig = {
     twitter: 'https://x.com/rami__maalouf',
     github: 'https://github.com/psycho-baller/convo-topics',
     waitlist: 'https://survey.typeform.com/to/erptGdCr',
-    discord: 'https://discord.gg/bK98qN2mxm',
+    discord: 'https://discord.gg/m9mWXcrstw',
     kofi: 'https://ko-fi.com/ramimaalouf',
     linkedin: 'https://www.linkedin.com/in/rami-m',
     youtube: 'https://youtube.com/@ramimaalouf',
