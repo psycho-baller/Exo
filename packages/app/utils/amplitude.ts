6da3853368c00b0aba66c96deb14239c0ddf997f
Rami Maalouf <rami.rami@ucalgary.ca>
2025-05-30 15:09:17 +0300

    added amplitude provider

---
+++ packages/app/utils/amplitude.ts
@@ -0,0 +1,35 @@
+import { init, setUserId, track } from '@amplitude/analytics-react-native'
+import * as Device from 'expo-device'
+
+let isInitialized = false
+
+export async function initializeAmplitude(apiKey: string) {
+  console.log(`initialize with API key: ${apiKey}`)
+  if (isInitialized) return
+  init(apiKey)
+  const deviceId = Device.osInternalBuildId || Device.osBuildId || Device.deviceName || 'unknown-device'
+  setUserId(deviceId)
+  isInitialized = true
+}
+
+interface TrackQuestionEventParams {
+  questionId: string
+  groupIds?: string[]
+  topicIds?: string[]
+  date?: string
+}
+
+export async function trackCreateQuestion(params: TrackQuestionEventParams) {
+  console.log("trackCreateQuestion", params)
+  await track('create_question', params)
+}
+
+export async function trackViewQuestion(params: TrackQuestionEventParams) {
+  console.log("trackViewQuestion", params)
+  await track('view_question', params)
+}
+
+export async function trackEditQuestion(params: TrackQuestionEventParams) {
+  console.log("trackEditQuestion", params)
+  await track('edit_question', params)
+}
