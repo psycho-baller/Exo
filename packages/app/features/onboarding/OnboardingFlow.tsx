b2227825e3021b67a22227092221ea30029cedac
Rami Maalouf <rami.rami@ucalgary.ca>
2025-06-13 04:34:02 +0400

    added screen 2

--- packages/app/features/onboarding/OnboardingFlow.tsx
+++ packages/app/features/onboarding/OnboardingFlow.tsx
@@ -1,9 +1,9 @@
 import type React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { OnboardingScreen1 } from './OnboardingScreen1';
+import { OnboardingScreen2 } from './OnboardingScreen2';
 import { useState } from 'react';
 // Import other screens as they're created
-// import { OnboardingScreen2 } from './OnboardingScreen2';
 // import { OnboardingScreen3 } from './OnboardingScreen3';
 // import { OnboardingScreen4 } from './OnboardingScreen4';
 
@@ -13,9 +13,10 @@ interface OnboardingFlowProps {
 
 export const OnboardingFlow: React.FC<OnboardingFlowProps> = ({ onComplete }) => {
   const [currentScreen, setCurrentScreen] = useState(1);
+  const totalScreens = 2; // Update this when adding more screens
 
   const handleNext = () => {
-    if (currentScreen < 1) {
+    if (currentScreen < totalScreens) {
       setCurrentScreen(currentScreen + 1);
     } else {
       onComplete();
@@ -26,8 +27,8 @@ export const OnboardingFlow: React.FC<OnboardingFlowProps> = ({ onComplete }) =>
     switch (currentScreen) {
       case 1:
         return <OnboardingScreen1 onNext={handleNext} />;
-      // case 2:
-      //   return <OnboardingScreen2 onNext={handleNext} />;
+      case 2:
+        return <OnboardingScreen2 onNext={handleNext} />;
       // case 3:
       //   return <OnboardingScreen3 onNext={handleNext} />;
       // case 4:
