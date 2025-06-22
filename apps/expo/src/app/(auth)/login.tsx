055f8e9badfc8ad2a956c7f6af2b1c969bd14cbd
Rami Maalouf <rami.rami@ucalgary.ca>
2025-06-23 00:22:26 +0400

    fix route for auth

--- apps/expo/src/app/(auth)/login.tsx
+++ apps/expo/src/app/(auth)/login.tsx
@@ -2,12 +2,12 @@ import { Redirect } from 'expo-router'
 import type { ComponentPropsWithoutRef, FC } from 'react'
 
 // If I wanna have an AuthProvider instead: https://github.com/fredrikburmester/expo-router-3-tab-example-with-auth/blob/main/context/AuthProvider.tsx
-interface Props extends ComponentPropsWithoutRef<'div'> {}
+interface Props extends ComponentPropsWithoutRef<'div'> { }
 
 const Component: FC<Props> = (props) => {
-  const {} = props
+  const { } = props
 
-  return <Redirect href='/(app)/(tabs)/questions' />
+  return <Redirect href='/(tabs)/questions' />
 }
 
 export default Component
